6a50c8a42ebd3210cf73f668d20c658e65a615e0
Mithun Banerjee <mithunbanerjee632@gmail.com>
2024-10-01 16:34:03 +0600

    user input

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+   int num;
+   string name;
+
+   cout <<"Enter the name: ";
+   cin >> name;
+   cout <<"Name is: "<<name <<endl;
+   cout <<"Please Enter the Mobile Number: ";
+   cin  >> num;
+   cout <<"The Mobile Number is: "<<num ;
+
+   getch();
+}
