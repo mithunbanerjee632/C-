b89f11c967ddba87bc48d7939927d2b197d92340
Mithun Banerjee <mithunbanerjee632@gmail.com>
2024-10-02 16:50:11 +0600

    showpoint,noshowpoint,precision,fixed

--- main.cpp
+++ main.cpp
@@ -1,34 +1,40 @@
 #include<iostream>
 #include<conio.h>
+#include<iomanip>
+
 using namespace std;
+
 int main(){
-   int num1,num2;
+   float num1,num2;
    cout <<"Input the Number1: ";
    cin >>num1;
 
     cout <<"Input the Number2: ";
    cin >>num2;
 
-   int sum = num1+num2;
+   cout <<showpoint; //showpoint:jokhon use kora hobe tarpor theke output .soho 6 ghor dekhabe
+
+   float sum = num1+num2;
    cout <<"The Sum is : "<<sum;
    cout <<endl;
 
-   int sub = num1-num2;
+   cout <<noshowpoint; //noshowpoint:jokhon use kora hobe tarpor theke output doshomik  dekhabe na
+
+   float sub = num1-num2;
    cout <<"The Subtraction is: "<<sub;
    cout <<endl;
 
-   int mul = num1*num2;
+   cout << fixed; // doshomiker por ghor gula dekhabe ja setpercision e j koighor set kora thakbe
+   cout <<setprecision(5); //setprecision:jokhon use kora hobe tarpor theke output doshomik soho 10 ghor  dekhabe na
+   float mul = num1*num2;
    cout <<"The Multiplication is: "<<mul;
    cout <<endl;
 
    double div = (float)num1/num2;   //type casting:declare datatype onno datatype e convert kora
-   cout <<"The Multiplication is: "<<div;
+   cout <<"The Division is: "<<div;
    cout <<endl;
 
-   double rem = num1%num2;
-   cout <<"The Remainderr is: "<<rem;
-   cout <<endl;
-   cout << "thats it!";
+
 
 
    getch();
