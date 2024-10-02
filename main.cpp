494d7d37dbaf5a6ad4c6da11db624f9dedccae56
Mithun Banerjee <mithunbanerjee632@gmail.com>
2024-10-02 16:30:31 +0600

    Data operation and type casting

--- main.cpp
+++ main.cpp
@@ -2,15 +2,34 @@
 #include<conio.h>
 using namespace std;
 int main(){
-   int num;
-   string name;
+   int num1,num2;
+   cout <<"Input the Number1: ";
+   cin >>num1;
 
-   cout <<"Enter the name: ";
-   cin >> name;
-   cout <<"Name is: "<<name <<endl;
-   cout <<"Please Enter the Mobile Number: ";
-   cin  >> num;
-   cout <<"The Mobile Number is: "<<num ;
+    cout <<"Input the Number2: ";
+   cin >>num2;
+
+   int sum = num1+num2;
+   cout <<"The Sum is : "<<sum;
+   cout <<endl;
+
+   int sub = num1-num2;
+   cout <<"The Subtraction is: "<<sub;
+   cout <<endl;
+
+   int mul = num1*num2;
+   cout <<"The Multiplication is: "<<mul;
+   cout <<endl;
+
+   double div = (float)num1/num2;   //type casting:declare datatype onno datatype e convert kora
+   cout <<"The Multiplication is: "<<div;
+   cout <<endl;
+
+   double rem = num1%num2;
+   cout <<"The Remainderr is: "<<rem;
+   cout <<endl;
+   cout << "thats it!";
+
 
    getch();
 }
